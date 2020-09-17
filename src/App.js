20b15ae19b053d9ee23a02c8f53c1c7bdbb0812a
William <wwantoine@gmail.com>
2020-09-17 11:40:03 +0700

    update6

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default class App extends Component {
 
     this.state = {
       weather: null,
-      cityInput: "",
+      cityInput: null,
       isLoading: true
     }
   }
@@ -27,7 +27,7 @@ export default class App extends Component {
 
   getWeather = async (cityName, lat, lon) => {
     let apikey = process.env.REACT_APP_APIKEY;
-    if(this.state.cityInput !== ""){
+    if(this.state.cityInput !== null){
       cityName=this.state.cityInput;
     }
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`
@@ -70,10 +70,18 @@ export default class App extends Component {
             <h1 className="col-12 display-4 my-2 py-3">{this.state.weather.name}, {this.state.weather.sys.country}</h1>
             <h2 className="col-12 text-danger">{this.state.weather.main.temp}°C</h2>
             <h2 className="col-12"><img id="weather-icon" src={`https://openweathermap.org/img/wn/${this.state.weather.weather[0].icon}@2x.png`} alt="" />{this.state.weather && this.state.weather.weather[0].description}</h2>
-            <button className="city-button" onClick={() => this.getWeather("paris", "", "")}>Paris</button>
-            <button className="city-button" onClick={() => this.getWeather("london", "", "")}>London</button>
-            <button className="city-button" onClick={() => this.getWeather("New York", "", "")}>New York</button>
-            <button className="city-button" onClick={() => this.getWeather("Tokyo", "", "")}>Tokyo</button>
+            <button className="city-button" onClick={() => {
+              this.getWeather("paris", "", "");
+              this.setState({cityInput: null})}}>Paris</button>
+            <button className="city-button" onClick={() => {
+              this.getWeather("london", "", "");
+              this.setState({cityInput: null})}}>London</button>
+            <button className="city-button" onClick={() => {
+              this.getWeather("new york", "", "");
+              this.setState({cityInput: null})}}>New York</button>
+            <button className="city-button" onClick={() => {
+              this.getWeather("tokyo", "", "");
+              this.setState({cityInput: null})}}>Tokyo</button>
           </div>
         </div>
       </div>
